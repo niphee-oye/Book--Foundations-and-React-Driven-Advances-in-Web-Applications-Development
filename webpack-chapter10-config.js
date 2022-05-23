28cae880061fe5d7bf3e517c6f14c1f1761812c3
niphee-oye <oluwanifemi.oyewole@pau.edu.ng>
2022-05-23 18:28:24 +0100

    Chapter 10

--- webpack-chapter10-config.js
+++ webpack-chapter10-config.js
@@ -16,5 +16,32 @@ module.exports = {
     output: { //The output.filename indicates that the built file will be named main.js and output.path shows that the path dist/chapter10/js needs to be resolved because the main.js file will be placed there
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist/chapter10/js')
+    },
+
+    // we need to add some rules (within module.rules) which can modify how the module is created. The rules include pointing use.loader element to babel-loader as shown below. The rules.test contains a regular expression which has to be tested. Here, what is indicated is that the rules should apply to .js files. We have also indicated using rules.exclude that we don’t want the rules to apply to node_modules directory
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
     }
 };
+
+/*
+Finally, we need to ensure that we can run the webpack script from command line by modifying the "script" section of our package.json file to include "build": "webpack" like that shown below. 
+
+"scripts": { 
+ "test": "echo \"Error: no test specified\" && exit 1", 
+ "build": "webpack" 
+ },
+
+*/
