2f98101eff36b45505418db36d6fe5c93da7b0a3
niphee-oye <oluwanifemi.oyewole@pau.edu.ng>
2022-05-24 08:28:55 +0100

    Pre ES6+ Commits

---
+++ src/chapter10/index.js
@@ -0,0 +1,5 @@
+(function(){
+    document.getElementById("output").innerHTML = "Hello Chapter 10"
+})();
+// The above function will simply check the HTML element whose id equals 'output' and set the inner HTML content to "Hello Chapter 10". Enclosing the whole function in a bracket — i.e. (function() ) — and adding another bracket at the end of function declaration causes the function to be auto-executed without the need to be called first. Such functions are conventionally referred to as immediately invoked function expression (IIFE), pronounced iffy.
+
